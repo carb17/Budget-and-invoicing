e7504ad9f7bc3ba61c4e3a23ca18353ffb82fc1e
carla beceiro <163188876+carb17@users.noreply.github.com>
2025-07-04 04:51:03 -0300

    Create types.tsx

---
+++ src/components/common/buttons/types.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  text?: string;
+  link?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  icon?: ReactNode;
+  className?: string;
+  children?: ReactNode;
+}
