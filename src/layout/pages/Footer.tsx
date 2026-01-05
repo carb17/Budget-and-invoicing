6ba7b4c631b53c5b61f45a4c43c022c87e3a0204
Carla Beceiro <carlabeceiro17@gmail.com>
2026-01-04 23:45:59 -0300

    arregle nombre

--- src/layout/pages/Footer.tsx
+++ src/layout/pages/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className='footer'>
       <div className='footer__container'>
         <p className='footer__text'>
-          © {new Date().getFullYear()} Electro BG · Desarrollo web:{' '}
+          © {new Date().getFullYear()} Accesorios Diamante · Desarrollo web:{' '}
           <a
             href={PORTAFOLIO_CARLA}
             target='_blank'
